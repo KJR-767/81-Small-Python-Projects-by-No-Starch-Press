81d929e9dc44e20b4c2c3675981b9d1ade9f6b70
KJR-767 <149409560+KJR-767@users.noreply.github.com>
2023-11-13 08:44:43 +0000

    Version 0.5

--- Bagels.py
+++ Bagels.py
@@ -1,13 +1,3 @@
-'''In Bagels, a deductive logic game, you
-must guess a secret three-digit number
-based on clues. The game offers one of
-the following hints in response to your guess:
-“Pico” when your guess has a correct digit in the
-wrong place, “Fermi” when your guess has a correct
-digit in the correct place, and “Bagels” if your guess
-has no correct digits. You have 10 tries to guess the
-secret number.'''
-
 import random
 
 NUM_DIGITS = 3
@@ -82,6 +72,4 @@ def getClues(guess, secretNum):
     
 if __name__ == '__main__':
         main()
-
-         
-
+        
